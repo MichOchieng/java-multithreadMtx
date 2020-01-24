2383233a6f8ec3c1db7bc73a77bbac446f63f8a1
MichOchieng <MichOchieng@gmail.com>
2020-01-24 02:37:15 -0800

    Added functionality for 'n' number of threads

--- threads.java
+++ threads.java
@@ -1,32 +1,41 @@
 public class threads extends Thread{
-    private int i0;
-    private int i1;
-    private int x;
-    private int y;
+    private  int x;
+    private  int y;
     private static int threadCount = 0;
+    private static int remThrd = 0;
+    private static int i0 = 0;
 
-    public threads(int i0,int i1,int x, int y){      
-        this.i0 = i0;
-        this.i1 = i1;
-        this.x  = x;
-        this.y  = y;
+    public threads(int x,int y){     
+        this.x =x;
+        this.y =y;
         threadCount++;
+        remThrd++;
         System.out.println("Started thread #" + threadCount);
     }
 
     @Override
-    public void run(){
-        System.out.println("Running thread");
-    }
+    public void run(){}
 
+    
     public void test(int[][]a, int[][]b,int[][]c){
-        mtxMult(i0,i1,x,y,a,b,c);
+        mtxMult(x,y,a,b,c);
     }
 
-    private static void mtxMult(int i0,int i1,int x, int y,int[][]a, int[][]b,int[][]c){
-        int sum;
-        for(int i = i0; i<x;i++){
-            for(int j = i1; j<x;j++){ 
+    private static void mtxMult(int x, int y,int[][]a, int[][]b,int[][]c){
+        int sum;       
+        int n; 
+        // Check for to see if the last thread has been reached
+        if(remThrd == 1){
+            n = x;
+        }else{
+            n = ((x/ threadCount + i0));
+        }        
+        
+        for(int i = i0; i<n; i++){  
+            // Debug          
+            System.out.println("i:" + i );
+            System.out.println("x:" + n);
+            for(int j = 0; j<x;j++){ 
                 sum = 0;               
                 for(int k = 0; k<y; k++){
                     sum += (a[i][k] * b[k][j]);
@@ -34,6 +43,10 @@ public class threads extends Thread{
                 c[i][j] = sum;
             }
         }
+        // Increments where the next thread starts from and decrements the number of thread reamaining
+        i0++;
+        remThrd--;
+        System.out.println();
 
     }
 
