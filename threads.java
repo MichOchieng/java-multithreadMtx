ee30389ab71c97f8239414c18e1cd56af8b67694
MichOchieng <MichOchieng@gmail.com>
2020-01-24 14:54:11 -0800

    Fixed issue with more threads than 'n' number of rows

--- threads.java
+++ threads.java
@@ -1,52 +1,69 @@
 public class threads extends Thread{
-    private  int x;
+    private static int x;
     private  int y;
     private static int threadCount = 0;
     private static int remThrd = 0;
     private static int i0 = 0;
+    private int id;
 
     public threads(int x,int y){     
         this.x =x;
         this.y =y;
+        id = threadCount;
         threadCount++;
         remThrd++;
-        System.out.println("Started thread #" + threadCount);
+        System.out.println("Created thread #" + (threadCount-1));
     }
 
     @Override
-    public void run(){}
+    public void run(){
+        System.out.println("Started thread #" + id);
+    }
 
-    
+
     public void test(int[][]a, int[][]b,int[][]c){
         mtxMult(x,y,a,b,c);
     }
-
-    private static void mtxMult(int x, int y,int[][]a, int[][]b,int[][]c){
-        int sum;       
-        int n; 
-        // Check for to see if the last thread has been reached
-        if(remThrd == 1){
-            n = x;
-        }else{
-            n = ((x/ threadCount + i0));
-        }        
-        
-        for(int i = i0; i<n; i++){  
-            // Debug          
-            System.out.println("i:" + i );
-            System.out.println("x:" + n);
-            for(int j = 0; j<x;j++){ 
-                sum = 0;               
-                for(int k = 0; k<y; k++){
-                    sum += (a[i][k] * b[k][j]);
-                }
-                c[i][j] = sum;
-            }
+    
+    private static void threadCheck(){
+        // Makes sure number of threads used at most matches number of row opperations
+        if(x<threadCount){            
+            threadCount = x;
+            remThrd = threadCount;            
         }
-        // Increments where the next thread starts from and decrements the number of thread reamaining
-        i0++;
-        remThrd--;
-        System.out.println();
+    }
+
+    private static void mtxMult(int x, int y,int[][]a, int[][]b,int[][]c){        
+        if(remThrd>0){ // Stops extra threads from attempting to multiply finished mtx
+            threadCheck();
+            int sum;       
+            int n; 
+            // Check to see if the last thread has been reached
+            switch(remThrd){
+                case 1:
+                    n = x;                    
+                    break;
+                default:  
+                    n = ((x/ threadCount + i0));  
+            }        
+            
+            for(int i = i0; i<n; i++){  
+                // Debug          
+                // System.out.println("i:" + i );
+                // System.out.println("n:" + n);
+                for(int j = 0; j<x;j++){ 
+                    sum = 0;               
+                    for(int k = 0; k<y; k++){
+                        sum += (a[i][k] * b[k][j]);
+                    }
+                    c[i][j] = sum;
+                }
+            }
+            // Increments where the next thread starts from and decrements the number of thread reamaining
+            i0++;
+            remThrd--;            
+            // System.out.println();
+        }
 
     }
 
