736ef12dd17f287df2f414612dad00515c0c9b02
MichOchieng <MichOchieng@gmail.com>
2020-01-26 00:24:56 -0800

    Created a method to create and store n number of threads

--- main.java
+++ main.java
@@ -1,64 +1,58 @@
 import java.util.Random;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class main{
     private static int x = 3;
     private static int y = 2;
-    private static int[][] mtx  = new int[x][y];
-    private static int[][] mtx1 = new int[y][x];
-    private static int[][] resultMtx   = new int[x][x];
+    private static int[][] mtx                = new int[x][y];
+    private static int[][] mtx1               = new int[y][x];
+    private static int[][] resultMtx          = new int[x][x];
+    private static ArrayList<threads> thrdLst = new ArrayList<threads>();
 
     public static void main(String[] args) {        
-        threads t = new threads(x,y);    
-        threads t2 = new threads(x,y);    
-        // threads t3 = new threads(x,y);  
-        // threads t4 = new threads(x,y);       
-        // threads t5 = new threads(x,y);    
-        // threads t6 = new threads(x,y);    
-        // threads t7 = new threads(x,y);  
-        // threads t8 = new threads(x,y);  
-        fillMtx();      
-        t.start();
-        t2.start(); 
-        // t3.start(); 
-        // t4.start(); 
-        // t5.start(); 
-        // t6.start(); 
-        // t7.start(); 
-        // t8.start();        
-        t.test(mtx,mtx1,resultMtx); 
-        t2.test(mtx,mtx1,resultMtx);
-        // t3.test(mtx,mtx1,resultMtx);
-        // t4.test(mtx,mtx1,resultMtx);
-        // t5.test(mtx,mtx1,resultMtx); 
-        // t6.test(mtx,mtx1,resultMtx);
-        // t7.test(mtx,mtx1,resultMtx);
-        // t8.test(mtx,mtx1,resultMtx);
+        fillMtx();       
+        long start = System.currentTimeMillis();
+        createThreads(1);
+        long end = System.currentTimeMillis();
+        long totalTime = end-start;
+        System.out.println("Total time: " + totalTime + "ms."); 
         view();
         view2();
         view3();    
     }    
 
+    // Creates n # of threads and starts multiplying
+    private static void createThreads(int n){
+        for(int i = 0; i<n;i++){
+            thrdLst.add(new threads(x, y,mtx,mtx1,resultMtx));
+            thrdLst.get(i).start();
+        }        
+        for(int i=0; i<n; i++){           
+            try{            
+                thrdLst.get(i).join();            
+             }catch (InterruptedException e){           
+               System.out.println("Error: Thread Interupted");            
+             }
+        }  
+    }
+    
     // Creates test matricies mtx and mtx1
     private static void fillMtx(){
         Random r = new Random();        
         for(int i = 0;i<y;i++){
-            for(int j = 0; j<x; j++){
-                // Change to random int
-                mtx1[i][j] = 1;                
+            for(int j = 0; j<x; j++){                
+                mtx1[i][j] = r.nextInt(100);                
             }
         }
-
         for(int i = 0;i<x;i++){
-            for(int j = 0; j<y; j++){
-                // Change to random int
-                mtx[i][j] = 1;                
+            for(int j = 0; j<y; j++){                
+                mtx[i][j] = r.nextInt(100);             
             }
         }
     }    
 
     // For viewing matrix after initialization / multiplication
-
     private static void view(){
         int n = mtx.length;
         for(int i = 0;i<x;i++){
